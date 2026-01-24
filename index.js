e7ae46fcd9f77ddcc45886c2b53d3d75004dcf64
Antônio Carlos Junior <a.carlosjr13@gmail.com>
2026-01-24 02:19:56 -0300

    Primeiro commit com código do Pai! Desafio resolvido com sucesso!

---
+++ index.js
@@ -0,0 +1,35 @@
+let nome = "Charmander";
+let tipo = "Fogo";
+let xp = 10000;
+let nivel = "Radiante";
+
+switch(true){
+    case(xp < 1000):
+      nivel= "Ferro";
+        break;
+    case(xp >= 1001 && xp <=2000):
+    nivel= "Bronze";
+        break;
+    case(xp >= 2001 && xp <= 5000):
+    nivel= "Prata";
+        break;
+    case (xp >= 5001 && xp<= 7000):
+        nivel= "Ouro";
+        break;
+    case (xp >= 7001 && xp <= 8000):
+        nivel= "Platina";
+        break;
+    case (xp >= 8001 && xp <= 9000):
+        nivel= "Ascendente";
+        break;
+    case (xp >= 9001 && xp <= 10000):
+        nivel= "Imortal";
+        break;
+    default:
+        nivel= "Radiante"; 
+    }       
+        console.log("Pokemon " + nome + " é do tipo " + tipo + " e está no nivel " + nivel + " com " + xp + " pontos de experiencia"); 
+    
+
+
+
